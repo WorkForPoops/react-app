6b50b5a4552e9f226d91b98a6f865d75d6b38a22
vagon_straponov <Zelinov_1097@mail.ru>
2020-03-23 02:10:21 +0300

    Updates

---
+++ precache-manifest.407885dc426ad4e4a7a6f74f86405e22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af677fd0433b1efded1cdcabdc92ba26",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "aa9ef97ac234129abf3d",
+    "url": "/react-app/static/css/main.10312e38.chunk.css"
+  },
+  {
+    "revision": "0573f3e2c68f2c01abfd",
+    "url": "/react-app/static/js/2.84ebaf5c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-app/static/js/2.84ebaf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9ef97ac234129abf3d",
+    "url": "/react-app/static/js/main.0dca3b82.chunk.js"
+  },
+  {
+    "revision": "fcde7d6d0533ef633e76",
+    "url": "/react-app/static/js/runtime-main.ec533adf.js"
+  },
+  {
+    "revision": "b378758415f3bbd8a56302a819d8bd55",
+    "url": "/react-app/static/media/cow.b3787584.png"
+  },
+  {
+    "revision": "5abe215e0aecd821778cb9ddb93fb744",
+    "url": "/react-app/static/media/milk.5abe215e.png"
+  },
+  {
+    "revision": "22f7e78647adb069830c20ea34709997",
+    "url": "/react-app/static/media/wheat.22f7e786.png"
+  }
+]);
